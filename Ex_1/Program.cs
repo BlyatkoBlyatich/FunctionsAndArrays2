e20d04b67e9e0954954b06f6a8628f036d137303
Василий Виссаров <vissar2016@rambler.ru>
2022-10-19 16:44:58 +0300

    Ex_1 done

---
+++ Ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int TwoDigit(int number)
+{
+    int des = number /10 % 10;
+
+    return des;
+}
+
+Console.WriteLine("Input Three digit-number");
+int result = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(TwoDigit(result));
+
